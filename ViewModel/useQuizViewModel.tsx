611b47dba048f4f0af7f9147dc449e723b6907ad
iP4uL0 <paulobapsantos@gmail.com>
2025-09-09 16:00:10 -0300

    Quiz

--- ViewModel/useQuizViewModel.tsx
+++ ViewModel/useQuizViewModel.tsx
@@ -1,86 +1,23 @@
+import { quizSchema, QuizType } from "@/schemas/quiz.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Resolver, useForm } from "react-hook-form";
-import { quizSchema } from "@/schemas/quiz.schema";
-import { z } from "zod";
-import axios from "axios";
-
-export type QuizType = z.infer<typeof quizSchema>;
-
-interface Pergunta {
-  id: string;
-  pergunta: string;
-  status: string;
-}
+import { useQuery } from "@tanstack/react-query";
+import { fecthPerguntas } from "@/service/quiz.service";
+import { useSnackBarContext } from "@/context/snackbar.context";
 
 export const useQuizViewModel = () => {
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-  const [loadingQuestions, setLoadingQuestions] = useState<boolean>(true);
-  const [questions, setQuestions] = useState<Pergunta[]>([]);
+    const { notify } = useSnackBarContext();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-    control,
-    reset,
-    setValue,
-    getValues,
-  } = useForm<QuizType>({
-    resolver: zodResolver(quizSchema) as unknown as Resolver<QuizType>,
-    defaultValues: {
-      nome: "",
-      
-      respostas: [],
-      score: undefined,
-    },
-  });
+    const {data, isError, error} = useQuery({
+        queryKey: ["perguntas"],
+        queryFn: fecthPerguntas,
+        staleTime: 6000,
+    })
 
-  // 🔹 Buscar perguntas da API usando Axios
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get<Pergunta[]>("http://localhost:3000/perguntas");
-        const data = response.data;
-
-        setQuestions(data);
-
-        // Inicializa respostas no formulário
-        reset({
-          nome: "",
-         
-          respostas: data.map((q) => ({
-            perguntaId: q.id,
-            resposta: "",
-          })),
-          score: undefined,
-        });
-      } catch (error) {
-        console.error("Erro ao buscar perguntas:", error);
-      } finally {
-        setLoadingQuestions(false);
-      }
-    };
-
-    fetchQuestions();
-  }, [reset]);
-
-
-  return {
-    onsubmit,
-    isSubmitting,
-    loadingQuestions,
-    questions,
-    register,
-    control,
-    errors,
-    handleSubmit,
-    reset,
-    setValue,
-    getValues,
-    watch,
-  };
-};
-
-export default useQuizViewModel;
+    return {
+        perguntas: data ? data : [],
+        isError, 
+        error
+    }
+}
